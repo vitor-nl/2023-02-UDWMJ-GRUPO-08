a8f27063b177ef90e940e1e25b20092bb96ab96c
JMprates27 <juanmarcelo27@gmail.com>
2023-10-06 19:56:44 -0300

    Classe cliente

---
+++ client.py
@@ -0,0 +1,8 @@
+class Client():
+    def __init__(self, first_name, last_name, address, cellphone, email, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.cellphone = cellphone
+        self.email = email
+        self.gender = gender
