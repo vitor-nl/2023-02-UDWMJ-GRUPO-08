15a10d55a14d3d56f737165a6f09299b3d13c181
dbziv <isadoramacields@gmail.com>
2023-10-06 20:49:42 -0300

    Class Category

---
+++ category.py
@@ -0,0 +1,6 @@
+class Category ():
+    def_init_(self, name, description, date_fabrication, is_active)
+        self.name = name
+        self.description = description
+        self.date_fabrication = date_fabrication
+        self.is_active = is_active
