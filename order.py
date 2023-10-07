ceb64856fd09301c3f88396d5272138d70ceb635
dbziv <isadoramacields@gmail.com>
2023-10-06 21:18:23 -0300

    order

---
+++ order.py
@@ -0,0 +1,7 @@
+import client
+    class Order (client.Client):
+        def_init_(self, name, status, total_price , is_active)
+            self.name = name
+            self.status = status
+            self.total_price = total_price
+            self.is_active = is_active
