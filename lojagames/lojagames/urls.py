e99f2c4c610dca8bf0fc1b03f1ef29fc42d6bfe7
juanmarcelo27@gmail.com <juanmarcelo27@gmail.com>
2023-11-23 20:30:06 -0300

    procuts não finalizado

--- lojagames/lojagames/urls.py
+++ lojagames/lojagames/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('produtos/', include('products.urls', namespace='products')),
     path('users/', include('user.urls')),
 ]
