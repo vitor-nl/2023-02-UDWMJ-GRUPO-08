107ce7681a875ac3abbc32c45b466b7716977ca0
juanmarcelo27@gmail.com <juanmarcelo27@gmail.com>
2023-11-28 16:24:27 -0300

    continuação do games (ainda inacabado)

--- lojagames/apps/games/models.py
+++ lojagames/apps/games/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Games(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
+    review = models.CharField('Review', max_length=500) 
+    note = models.IntegerField('Nota')
     release_date = models.DateField('Data de Lançamento', auto_now=False, auto_now_add=False)
 
     class Meta:
--- lojagames/apps/games/urls.py
+++ lojagames/apps/games/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path, include
-from . import views
-from rest_framework import routers
+from games.views import game_list
 
 app_name = 'games'
-
-router = routers.DefaultRouter()
-router.register('games', views.ProductViewSet, basename='games')
-
 urlpatterns = [
-    path('', include(router.urls) )
+    path('list/', game_list)
 ]
