f061f5f69175f08404e3907cebc14aaf1adfdab7
juanmarcelo27@gmail.com <juanmarcelo27@gmail.com>
2023-11-29 18:59:42 -0300

    Tentativa para o Pull

--- lojagames/lojagames/settings.py
+++ lojagames/lojagames/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'user.apps.UserConfig',
     'games.apps.GamesConfig',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
+}
--- lojagames/lojagames/urls.py
+++ lojagames/lojagames/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('games/', include('games.urls')),
     path('users/', include('user.urls')),
+    path('games/', include('games.urls')),
 ]
