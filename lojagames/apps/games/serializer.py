5f83fc261e0898e996689f4d0e8d66629f3951ad
juanmarcelo27@gmail.com <juanmarcelo27@gmail.com>
2023-11-30 17:06:59 -0300

    criação do create_game

--- lojagames/apps/games/serializer.py
+++ lojagames/apps/games/serializer.py
@@ -1,7 +1,19 @@
 from games.models import Games
 from rest_framework import serializers
+from games.models import Games
 
 class GamesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Games
-        fields = '__all__'
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    description = serializers.CharField()
+    review = serializers.CharField() 
+    note = serializers.IntegerField()
+    release_date = serializers.DateField()
+    
+    def create(self, data):
+        return Games.objects.create(**data)
+
+
+    #class Meta:
+    #    model = Games
+    #    fields = '__all__'
