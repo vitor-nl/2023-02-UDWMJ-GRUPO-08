5a3a1f9fbd2281b7c4d611986fd9a47339695bf9
juanmarcelo27@gmail.com <juanmarcelo27@gmail.com>
2023-12-01 17:46:05 -0300

    games -> review

--- lojagames/apps/games/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from games.views import game_list, game_create
-
-app_name = 'games'
-urlpatterns = [
-    path('', game_create),
-    path('list/', game_list)
-]
