b70e11c8899ae5ee497061208192f1275285c6e9
Alvim1100 <eric.weber.alvim@gmail.com>
2023-12-05 20:21:07 -0300

    botão arrumado

--- gamelog/gamelog/urls.py
+++ gamelog/gamelog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('', include('user.urls')),
     path('review/', include('review.urls')),
     path('wishlist/', include('wishlist.urls')),
 ]
